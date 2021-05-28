905beb2212603657ed3166249460024e83ee13c5
mohammed mustafa <mohammed.alkhddar@gmail.com>
2021-05-27 23:11:14 -0500

    add redux

---
+++ src/components/layout/SearchBar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+// import PropTypes from 'prop-types';
+
+export const SearchBar = () => {
+    
+    return (
+        <nav style={{ marginBottom: '30px' }} className='blue'>
+      <div className='nav-wrapper'>
+        <form>
+          <div className='input-field'>
+            <input
+              id='search'
+              type='search'
+              placeholder='Search Logs..'
+             
+            />
+            <label className='label-icon' htmlFor='search'>
+              <i className='material-icons'>search</i>
+            </label>
+            <i className='material-icons'>close</i>
+          </div>
+        </form>
+      </div>
+    </nav>
+
+    )
+}
+
+
+export default SearchBar;
---
+++ src/components/techs/TechItem.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+// import { deleteTech } from '../../actions/techActions';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const TechItem = ({ tech }) => {
+
+  const onDelete = () => {
+  
+    M.toast({ html: 'Technician deleted' });
+  };
+
+  return (
+    <li className='collection-item'>
+      <div>
+        {tech.firstName} {tech.lastName}
+        <a href='#!' className='secondary-content' onClick={onDelete}>
+          <i className='material-icons grey-text'>delete</i>
+        </a>
+      </div>
+    </li>
+  );
+};
+
+TechItem.propTypes = {
+  tech: PropTypes.object.isRequired,
+ 
+};
+
+export default TechItem;
